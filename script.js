0bfb199dd764191b9ff5c0fa428a529314753be4
Craig Clayton <craig__clayton@outlook.com>
2024-12-08 20:57:22 +0000

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// document.body.addEventListener('click', () => {
-//   const audio = new Audio('sound/fly-129618.mp3');
-//   audio.play();
-// });
+document.body.addEventListener('click', () => {
+  const audio = new Audio('sound/fly-129618.mp3');
+  audio.play();
+});
 
 // https://dobrian.github.io/cmp/topics/sample-recording-and-playback-with-web-audio-api/1.loading-and-playing-sound-files.html#:~:text=Method%202%3A%20JavaScript,back%20the%20sound%20with%20the%20.
